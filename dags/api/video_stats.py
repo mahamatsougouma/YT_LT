29edd85899f2eed94d303abb62bccf78e24e9a81
Mahamat Sougouma Ali <mahamatsougouma367@gmail.com>
2026-09-14 16:07:26 +0100

    Add Docker Airflow ELT pipeline and tests

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,17 +1,23 @@
 import requests
 import json
 from datetime import date
-import os
-from dotenv import load_dotenv
-load_dotenv(dotenv_path="./.env")
-API_KEY = os.getenv("API_KEY")
+#import os
+#from dotenv import load_dotenv
+#load_dotenv(dotenv_path="./.env")
+
+from airflow.decorators import task
+from airflow.models import Variable
 
 
-CHANNEL_HANDLE ="MrBeast"
+
+API_KEY = Variable.get("API_KEY")
+
+
+CHANNEL_HANDLE =Variable.get("CHANNEL_HANDLE")
 maxResults = 50 
 
 
-
+@task
 def get_playlist_id():
     try:
 
@@ -41,7 +47,7 @@ def get_playlist_id():
 
 
 
-
+@task
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -81,7 +87,7 @@ def get_video_ids(playlistId):
         raise e
 
     
-
+@task
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -126,7 +132,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
